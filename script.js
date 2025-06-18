3109e85de553fea193542d6e10b8ccd2c702087b
edzhang10 <edzhang10@gmail.com>
2025-06-18 13:58:32 -0400

    jizdy 💀

--- script.js
+++ script.js
@@ -1,25 +1,85 @@
-let baby_oil = 0
+let baby_oil = 0;
+let oil_grabber = 0;
+let grabber_rate = 1;
+let party = 0;
+let oil_per_click = 1;
+let grabber_cost = 10;
+let upgrade_cost = 50;
+let party_cost = 100;
+let countdown = 30;
+document.getElementById('nav-jizdy').innerText = `Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown}  `;
+
 
 function playAudio() {
-    let audio = new Audio("diddyparty.mp3");
+    let audio = new Audio("/assets/button.mp3");
     audio.play();
 }
 
 function updateOil() {
-    document.getElementById('baby_oil').innerText = baby_oil;
+    document.getElementById('oil').innerText = baby_oil;
+    new_oil = document.createElement('img')
+    new_oil.setAttribute('src', '/assets/baby-oil.png'), Object.assign(new_oil.style, { height: '5vh', width: '5vw', position: 'absolute', zIndex: '-1000'});
+    new_oil.style.left = `${Math.floor(Math.random() * 100)}%`
+    new_oil.style.top = `${Math.floor(Math.random() * 100)}%`
+    document.getElementById('column-two').appendChild(new_oil)
 }
 
 async function getOil() {
-    playAudio()    
+    playAudio()
     console.log('hi')
-    await new Promise(r => setTimeout(r, 100));
-    baby_oil += 1
-    new_oil = document.createElement('img')
-    new_oil.setAttribute('src', '/images/baby-oil.png')
-    new_oil.style.left = `${Math.floor(Math.random() * 100)}%`
-    new_oil.style.top = `${Math.floor(Math.random() * 100)}%`
-    document.getElementById('baby_oils').appendChild(new_oil)
-    updateOil()
+    baby_oil += oil_per_click;
+    updateOil();
 }
 
-getOil()
+function upgradeClicks() {
+    if (baby_oil >= upgrade_cost) {
+        playAudio();
+        baby_oil -= upgrade_cost;
+        upgrade_cost = Math.floor(upgrade_cost * 1.1);
+        oil_per_click += 1;
+        document.getElementById('upgrade-clicks-cost').innerText = `(-${upgrade_cost})`;
+        document.getElementById('oil-per-click').innerText = `(+${oil_per_click})`;
+        document.getElementById('opc-value').innerText = `${oil_per_click}`;
+        updateOil();
+        }
+}
+
+function buyGrabber() {
+    if (baby_oil >= grabber_cost) {
+        playAudio();
+        baby_oil -= grabber_cost;
+        oil_grabber += 1;
+        grabber_cost = Math.floor(grabber_cost * 1.1);
+        document.getElementById('grabber-cost').innerText = `(-${grabber_cost})`;
+        document.getElementById('grabber-count').innerText = `${oil_grabber}`;
+        updateOil();
+    }
+}
+
+function hostParty() {
+    if (baby_oil >= party_cost) {
+        playAudio();
+        baby_oil -= party_cost;
+        party += 1;
+        party_cost = Math.floor(party_cost * 1.1);
+        document.getElementById('party-cost').innerText = `(-${party_cost})`;
+        document.getElementById('party-count').innerText = `${party}`;
+        grabber_rate += 1;
+        updateOil();
+    }
+}
+
+
+setInterval(() => {
+    if (oil_grabber > 0) {
+        baby_oil += grabber_rate * oil_grabber;
+        console.log(grabber_rate);
+        console.log(oil_grabber);
+        updateOil();
+    }
+}, 1000);
+
+setInterval(() => {
+    countdown -= 1;
+    document.getElementById('nav-jizdy').innerText = `Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown}  `;
+    }, 1000);
