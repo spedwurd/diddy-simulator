e1bd8a58132b7e81126af1900d85906886050db7
edzhang10 <edzhang10@gmail.com>
2025-06-18 22:37:56 -0400

    made some things work

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let oil_per_click = 1;
 let grabber_cost = 10;
 let upgrade_cost = 50;
 let party_cost = 100;
-let countdown = 30;
+let countdown = 5;
 document.getElementById('nav-jizdy').innerText = `Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown}  `;
 
 
@@ -18,7 +18,7 @@ function playAudio() {
 function updateOil() {
     document.getElementById('oil').innerText = baby_oil;
     new_oil = document.createElement('img')
-    new_oil.setAttribute('src', '/assets/baby-oil.png'), Object.assign(new_oil.style, { height: '5vh', width: '5vw', position: 'absolute', zIndex: '-1000'});
+    new_oil.setAttribute('src', '/assets/baby-oil.png'), Object.assign(new_oil.style, { height: '5vh', width: '5vw', position: 'absolute', zIndex: '-1000', animation: 'fadeIn 1s'});
     new_oil.style.left = `${Math.floor(Math.random() * 100)}%`
     new_oil.style.top = `${Math.floor(Math.random() * 100)}%`
     document.getElementById('column-two').appendChild(new_oil)
@@ -37,7 +37,7 @@ function upgradeClicks() {
         baby_oil -= upgrade_cost;
         upgrade_cost = Math.floor(upgrade_cost * 1.1);
         oil_per_click += 1;
-        document.getElementById('upgrade-clicks-cost').innerText = `(-${upgrade_cost})`;
+        document.getElementById('upgrade-clicks-cost').innerText = `(-${upgrade_cost} oil)`;
         document.getElementById('oil-per-click').innerText = `(+${oil_per_click})`;
         document.getElementById('opc-value').innerText = `${oil_per_click}`;
         updateOil();
@@ -50,7 +50,7 @@ function buyGrabber() {
         baby_oil -= grabber_cost;
         oil_grabber += 1;
         grabber_cost = Math.floor(grabber_cost * 1.1);
-        document.getElementById('grabber-cost').innerText = `(-${grabber_cost})`;
+        document.getElementById('grabber-cost').innerText = `(-${grabber_cost} oil)`;
         document.getElementById('grabber-count').innerText = `${oil_grabber}`;
         updateOil();
     }
@@ -62,7 +62,7 @@ function hostParty() {
         baby_oil -= party_cost;
         party += 1;
         party_cost = Math.floor(party_cost * 1.1);
-        document.getElementById('party-cost').innerText = `(-${party_cost})`;
+        document.getElementById('party-cost').innerText = `(-${party_cost} oil)`;
         document.getElementById('party-count').innerText = `${party}`;
         grabber_rate += 1;
         updateOil();
@@ -75,11 +75,16 @@ setInterval(() => {
         baby_oil += grabber_rate * oil_grabber;
         console.log(grabber_rate);
         console.log(oil_grabber);
-        updateOil();
+        for (let i = 0; i < oil_grabber*grabber_rate; i++) {
+            updateOil();
+        }
     }
 }, 1000);
 
 setInterval(() => {
     countdown -= 1;
+    if (countdown <= 0) {
+        countdown = 30;
+    }
     document.getElementById('nav-jizdy').innerText = `Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown} Jizdy comes in ${countdown}  `;
     }, 1000);
